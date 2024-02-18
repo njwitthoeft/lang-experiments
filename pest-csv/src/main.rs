d72d20c85d672955ebb1634537b62eabd46789e7
Nicholas Witthoeft <njwitthoeft@gmail.com>
2024-02-18 15:08:07 -0600

    field parsing works

--- pest-csv/src/main.rs
+++ pest-csv/src/main.rs
@@ -1,3 +1,14 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "csv.pest"]
+pub struct CSVParser;
+
 fn main() {
-    println!("Hello, world!");
+    let good_parse = CSVParser::parse(Rule::field, "-273.15");
+    println!("{:?}", good_parse);
+
+    let bad_parse = CSVParser::parse(Rule::field, "not a number");
+    println!("{:?}", bad_parse);
 }

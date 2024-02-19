4ca4781449a5b7e1cbe774f81295b18a8384b838
Nicholas Witthoeft <njwitthoeft@gmail.com>
2024-02-18 18:03:07 -0600

    add json-ast roundtrip

--- pest-json/src/json.rs
+++ pest-json/src/json.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 use pest::error::Error;
-
+use pest::iterators::Pair;
+use pest::Parser;
 use pest_derive::Parser;
 
 #[derive(Parser)]
@@ -38,4 +39,57 @@ fn serialize_jsonvalue(val: &JSONValue) -> String {
     }
 }
 
-fn parse_json_file(file: &str) -> Result<JSONValue, Error<Rule>> {}
+#[allow(clippy::result_large_err)]
+fn parse_json_file(file: &str) -> Result<JSONValue, Error<Rule>> {
+    let json = JSONParser::parse(Rule::json, file)?.next().unwrap();
+
+    fn parse_value(pair: Pair<Rule>) -> JSONValue {
+        match pair.as_rule() {
+            Rule::object => JSONValue::Object(
+                pair.into_inner()
+                    .map(|pair| {
+                        let mut inner_rules = pair.into_inner();
+                        let name = inner_rules
+                            .next()
+                            .unwrap()
+                            .into_inner()
+                            .next()
+                            .unwrap()
+                            .as_str();
+                        let value = parse_value(inner_rules.next().unwrap());
+                        (name, value)
+                    })
+                    .collect(),
+            ),
+            Rule::array => JSONValue::Array(pair.into_inner().map(parse_value).collect()),
+            Rule::string => JSONValue::String(pair.into_inner().next().unwrap().as_str()),
+            Rule::number => JSONValue::Number(pair.as_str().parse().unwrap()),
+            Rule::boolean => JSONValue::Boolean(pair.as_str().parse().unwrap()),
+            Rule::null => JSONValue::Null,
+            Rule::json
+            | Rule::EOI
+            | Rule::pair
+            | Rule::value
+            | Rule::inner
+            | Rule::char
+            | Rule::WHITESPACE => unreachable!(),
+        }
+    }
+    Ok(parse_value(json))
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::*;
+
+    #[test]
+    fn roundtrip_json() {
+        let unparsed_file = fs::read_to_string("example.json").unwrap();
+        let json: JSONValue = parse_json_file(&unparsed_file).unwrap();
+
+        let expected_serialization = "{\"nesting\":{\"inner object\":{}},\"an array\":[1.5,true,null,0.000001],\"string with escaped double quotes\":\"\\\"quick brown foxes\\\"\"}";
+        assert_eq!(expected_serialization, serialize_jsonvalue(&json))
+    }
+}
--- pest-json/src/main.rs
+++ pest-json/src/main.rs
@@ -1,4 +1,5 @@
 mod json;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, JSON!");
 }
